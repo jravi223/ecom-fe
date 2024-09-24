6f4911ef3649334c8c60bcfe2a50eea66e910b32
Ravi J <jravi223@gmail.com>
2024-09-24 21:52:30 +0530

    add changes first changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { UserViewComponent } from '../users/user-view/user-view.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: UserViewComponent }
+];
